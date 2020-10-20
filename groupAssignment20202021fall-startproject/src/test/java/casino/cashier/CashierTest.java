826fd71dd7353f520c5666dfc839d871803251e6
Yoanna <yoanna.i.borisova@gmail.com>
2020-10-20 23:06:36 +0200

    Create Test CashierCanHandGamblerCardAndInformBetLoggingAuthority(). Note: Autogenerated implementations were made by IntelliJ for the entire project. Added dependencies in build.gradle

---
+++ groupAssignment20202021fall-startproject/src/test/java/casino/cashier/CashierTest.java
@@ -0,0 +1,27 @@
+package casino.cashier;
+
+import gamblingauthoritiy.IBetLoggingAuthority;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+
+public class CashierTest {
+
+    @Test
+    public void CashierCanHandGamblerCardAndInformBetLoggingAuthority()
+    {
+        IBetLoggingAuthority bla = mock(IBetLoggingAuthority.class);
+        Cashier testCashier = new Cashier(bla);
+
+        IGamblerCard tempCard = testCashier.distributeGamblerCard();
+        List<IGamblerCard> tempCardList = testCashier.getListOfGamblerCards();
+
+        Assert.assertTrue(tempCardList.contains(tempCard));
+        verify(bla.logHandOutGamblingCard(tempCard.getCardID()));
+    }
+
+}
