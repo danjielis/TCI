a7aea5c645bc665d20734a70498e3803488e1f7b
Dongdong Ke <d.ke@student.fontys.nl>
2020-10-23 20:34:06 +0200

    Create test for acceptBet

--- groupAssignment20202021fall-startproject/src/test/java/DefaultGameTest.java
+++ groupAssignment20202021fall-startproject/src/test/java/DefaultGameTest.java
@@ -1,8 +1,13 @@
+import casino.bet.Bet;
+import casino.bet.MoneyAmount;
 import casino.cashier.GamblerCard;
 import casino.cashier.IGamblerCard;
 import casino.game.BettingRound;
 import casino.game.DefaultGame;
 import casino.game.GameRule;
+import casino.game.NoCurrentRoundException;
+import casino.gamingmachine.IGamingMachine;
+import casino.idfactory.BetID;
 import gamblingauthoritiy.*;
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
@@ -13,6 +18,9 @@ public class DefaultGameTest {
     private IBetTokenAuthority betTokenAuthority = mock(IBetTokenAuthority.class);
     private IBetLoggingAuthority betLoggingAuthority= mock(gamblingauthoritiy.IBetLoggingAuthority.class);
     DefaultGame game=new DefaultGame(gameRule,betLoggingAuthority,betTokenAuthority);
+    MoneyAmount moneyAmount = mock(MoneyAmount.class);
+    private IGamingMachine gamingMachine = mock(IGamingMachine.class);
+
 
     /**
      * Check if the new bettingRound starts
@@ -20,7 +28,6 @@ public class DefaultGameTest {
      */
     @Test
     public void CheckIfNewBettingRoundStart(){
-        game=new DefaultGame(gameRule,betLoggingAuthority,betTokenAuthority);
         game.startBettingRound();
         verify(betLoggingAuthority).logStartBettingRound(any(BettingRound.class));
     }
@@ -28,5 +35,12 @@ public class DefaultGameTest {
     /**
      * Check
      */
+    @Test
+    public void acceptBetSuccessfully() throws NoCurrentRoundException {
+        game.startBettingRound();
+        MoneyAmount moneyAmount = mock(MoneyAmount.class);
+        Bet bet=new Bet(new BetID(),moneyAmount);
+        assertTrue(game.acceptBet(bet, gamingMachine));
+    }
 
 }
