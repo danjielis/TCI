75d91e180d949a162fc08614d61ace46fef3d88d
danjielis <danjielis@gmail.com>
2020-10-23 16:19:03 +0200

    Class: Card ID. Test count: 3. Tested behavior: Essentially it is only tested whether base class is functioning properly, since no functionality besides base, is attached to the card, requirements state that card must be anonymized and without bet tracking. TDD cycle 1.

---
+++ groupAssignment20202021fall-startproject/src/main/java/casino/cashier/CardIDTest.java
@@ -0,0 +1,41 @@
+package casino.cashier;
+
+import casino.idfactory.GeneralID;
+import casino.idfactory.IDFactory;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.instanceOf;
+
+public class CardIDTest {
+
+    @Test
+    public void IDFactoryCanCreateCardID() {
+        IDFactory testFactory = new IDFactory();
+
+        GeneralID temp = testFactory.generateID("IDCard");
+
+        assertThat(temp, instanceOf(CardID.class));
+    }
+
+    @Test
+    public void ShouldHaveUniqueIdentification() {
+        IDFactory testFactory = new IDFactory();
+
+        GeneralID temp = testFactory.generateID("IDCard");
+        GeneralID temp1 = testFactory.generateID("IDCard");
+
+        assertNotEquals(temp.getUniqueID(), temp1.getUniqueID());
+    }
+
+    @Test
+    public void ShouldHaveDifferentTimestamps() {
+        IDFactory testFactory = new IDFactory();
+
+        GeneralID temp = testFactory.generateID("IDCard");
+        GeneralID temp1 = testFactory.generateID("IDCard");
+
+        assertNotEquals(temp.getTimeStamp(), temp1.getTimeStamp());
+    }
+
+}
