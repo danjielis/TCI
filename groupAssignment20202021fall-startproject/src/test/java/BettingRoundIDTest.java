b5718bb26bc914f3e5165af5c3b7f67698c5a8f4
Dongdong Ke <d.ke@student.fontys.nl>
2020-10-24 14:14:42 +0200

    Move file position

---
+++ groupAssignment20202021fall-startproject/src/test/java/BettingRoundIDTest.java
@@ -0,0 +1,33 @@
+import casino.idfactory.BettingRoundID;
+import casino.idfactory.GeneralID;
+import casino.idfactory.IDFactory;
+import org.junit.Test;
+import org.junit.Assert;
+
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.junit.Assert.*;
+
+
+public class BettingRoundIDTest {
+
+    @Test
+    public void FactoryCreateBettingRoundID(){
+        GeneralID bettingRoundIDInstance= IDFactory.generateID("bettingRoundID");
+        assertThat(bettingRoundIDInstance, instanceOf(BettingRoundID.class));
+    }
+
+    @Test
+    public void FactoryShouldCreateUniqueBettingRoundID(){
+        GeneralID id1= IDFactory.generateID("bettingRoundID");
+        GeneralID id2= IDFactory.generateID("bettingRoundID");
+        assertNotEquals(id1.getUniqueID(),id2.getUniqueID());
+    }
+
+    @Test
+    public void FactoryShouldCreateUniqueTimeStamp() throws InterruptedException {
+        GeneralID id1= IDFactory.generateID("bettingRoundID");
+        Thread.sleep(100);
+        GeneralID id2= IDFactory.generateID("bettingRoundID");
+        assertNotEquals(id1.getTimeStamp(),id2.getTimeStamp());
+    }
+}
---
+++ groupAssignment20202021fall-startproject/src/test/java/GamingMachineIDTest.java
@@ -0,0 +1,32 @@
+import casino.gamingmachine.GamingMachineID;
+import casino.idfactory.GeneralID;
+import casino.idfactory.IDFactory;
+import org.junit.Test;
+
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertThat;
+
+public class GamingMachineIDTest {
+
+    @Test
+    public void FactoryCreateBettingRoundID(){
+        GeneralID gamingMachineIDIDInstance= IDFactory.generateID("gamingMachineID");
+        assertThat(gamingMachineIDIDInstance, instanceOf(GamingMachineID.class));
+    }
+
+    @Test
+    public void FactoryShouldCreateUniqueBettingRoundID(){
+        GeneralID id1= IDFactory.generateID("gamingMachineID");
+        GeneralID id2= IDFactory.generateID("gamingMachineID");
+        assertNotEquals(id1.getUniqueID(),id2.getUniqueID());
+    }
+
+    @Test
+    public void FactoryShouldCreateUniqueTimeStamp() throws InterruptedException {
+        GeneralID id1= IDFactory.generateID("gamingMachineID");
+        Thread.sleep(100);
+        GeneralID id2= IDFactory.generateID("gamingMachineID");
+        assertNotEquals(id1.getTimeStamp(),id2.getTimeStamp());
+    }
+}
