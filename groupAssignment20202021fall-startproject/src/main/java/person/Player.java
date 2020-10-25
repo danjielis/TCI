fc34fbe5ceb2f756448649aa249b19b3a6c45f13
danjielis <danjielis@gmail.com>
2020-10-25 20:17:28 +0100

    Class: Player. Test count: 1. Tested behavior: casino player should be a le to connect to a machine. TDD cycle stage 2 - added implementation.

--- groupAssignment20202021fall-startproject/src/main/java/person/Player.java
+++ groupAssignment20202021fall-startproject/src/main/java/person/Player.java
@@ -2,9 +2,13 @@ package person;
 
 import casino.cashier.GamblerCard;
 import casino.cashier.IGamblerCard;
+import casino.gamingmachine.GamingMachine;
+import casino.gamingmachine.IGamingMachine;
+import casino.idfactory.GamingMachineID;
 
 public class Player {
     private IGamblerCard connectedCard;
+    private GamingMachine gamingMachine;
 
     public void obtainGamblerCard(GamblerCard card) {
         this.connectedCard = card;
@@ -12,5 +16,13 @@ public class Player {
 
     public IGamblerCard getGamblerCard() {
         return this.connectedCard;
+    }
+
+    public void selectGamingMachine(GamingMachine gamingMachine) {
+        this.gamingMachine = gamingMachine;
+    }
+
+    public GamingMachineID selectedGamingMachineID() {
+        return this.gamingMachine.getGamingMachineID();
     }
 }
