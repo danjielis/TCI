8acde15f7d5c3dd6a8bfb097a58c720e95b6c2d8
danjielis <danjielis@gmail.com>
2020-10-25 18:00:13 +0100

    Class: Player. Test count: 1. Tested behavior: casino player should be able to get card before beginning to play. TDD cycle stage 1 - empty class and one test.

---
+++ groupAssignment20202021fall-startproject/src/main/java/person/PlayerTest.java
@@ -0,0 +1,35 @@
+package person;
+
+import casino.cashier.Cashier;
+import casino.cashier.GamblerCard;
+import gamblingauthoritiy.IBetLoggingAuthority;
+import gamblingauthoritiy.IBetTokenAuthority;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class PlayerTest {
+
+    private IBetLoggingAuthority loggingAuthority;
+
+    @Before
+    public void setUp() {
+        this.loggingAuthority = mock(IBetLoggingAuthority.class);
+    }
+
+    @Test
+    public void ShouldReceiveGamblerCard() {
+        Player player = new Player();
+        Cashier cashier = new Cashier(loggingAuthority);
+
+        GamblerCard card = (GamblerCard) cashier.distributeGamblerCard();
+        player.obtainGamblerCard(card);
+
+        assertEquals("cards must be equal", player.getGamblerCard(), card);
+        verify(loggingAuthority).logHandOutGamblingCard(card.getCardID());
+    }
+
+}
